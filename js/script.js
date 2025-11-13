31c366069f458c755637a719e3cff10fbce45183
Muhammed Humraz <muhammadhamraz201@gmail.com>
2025-11-13 14:12:24 +0530

    Update and rename script.js to js/script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let index = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide() {
+  slides.forEach((slide, i) => {
+    slide.style.transform = `translateX(-${index * 100}%)`;
+  });
+  index = (index + 1) % slides.length;
+}
+
+setInterval(showSlide, 4000);
